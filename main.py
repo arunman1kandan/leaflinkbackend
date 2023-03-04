4602b33136acd8a08150e64d6544110a2504e5d8
arunmanikandan <106937963+arunman1kandan@users.noreply.github.com>
2023-03-05 02:21:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import os
+from prediction import *
 
 app = Flask(__name__)
 
@@ -8,6 +9,14 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route("/getPlantDisease",methods=["POST"])
+def getPlantDiseaseData():
+	if(request.method=="POST"):
+		imageUrl = request.get_json()["imageURL"]
+		print(fetchResponse(imageUrl))
+		return fetchResponse(imageUrl)
+	else:
+		return f"{request.method} will not work";
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
